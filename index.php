127ca455d321a94a55db3a4f9648b70e76b1553f
dalaryizmi99 <62916535+dalaryizmi99@users.noreply.github.com>
2020-11-22 07:54:22 +0700

    push file

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Title Page</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="./assets/css/index.css" rel="stylesheet">
+    <!-- End Bootstrap CSS -->
+
+</head>
+
+<body>
+    <div class="all">
+        <div class="logoHeader">
+            <p class="logoHeader__1">DALARYIZMI</p>
+            <p class="logoHeader__2">WEBSITE</p>
+        </div>
+        <div class="bodyPage">
+            <div class="quote">
+                <pre id="typewriter">
+                    <span class="upperText">Sự vi diệu của code là:</span>
+                        hôm qua chạy tốt,
+                          hôm nay không được,
+                            nhưng ngày mai lại chạy bình thường....</pre>
+            </div>
+            <div class="login-form">
+                <!-- <head>
+                    <link
+                    rel="stylesheet"
+                    href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+                    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+                    crossOrigin="anonymous"
+                  />
+                  </head> -->
+
+                <form class="login-form" autocomplete="off" action="#">
+                    <input type="email" name="email" placeholder="Email" />
+                    <div class="input-icon">
+                        <input type="password" name="email" placeholder="Password" />
+                        <i class="fa fa-eye show-password"></i>
+                    </div>
+                    <a href="#" class="forgot">Forgot password?</a>
+                    <button>Sign in</button>
+                </form>
+            </div>
+        </div>
+        <div class="footerPage">
+            <p>Handcrafted by me</p>
+        </div>
+    </div>
+
+
+    <!-- jQuery -->
+    <script src="./assets/js/index.js"></script>
+</body>
+
+</html>
